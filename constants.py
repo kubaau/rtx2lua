1ecd3113013f5e148c65d1dd817054216ac31a1a
Jakub Audykowicz <jakub.audykowicz@gmail.com>
2024-07-17 22:54:26 +0200

    Adorn constants with their original ID's

--- constants.py
+++ constants.py
@@ -1,138 +1,138 @@
 animals = [
-    "SPEC_POLARBEAR",
-    "SPEC_RABBITWHITE",
-    "SPEC_RABBITGREY",
-    "SPEC_FOX",
-    "SPEC_STAG",
-    "SPEC_DEER",
-    "SPEC_DUCK",
-    "JOB_PACKDONKEY", #yes, in the original id 7 can be used to spawn donkeys, although it seems useless
-    "SPEC_SHEEP",
+    "SPEC_POLARBEAR",       #0
+    "SPEC_RABBITWHITE",     #1
+    "SPEC_RABBITGREY",      #2
+    "SPEC_FOX",             #3
+    "SPEC_STAG",            #4
+    "SPEC_DEER",            #5
+    "SPEC_DUCK",            #6
+    "JOB_PACKDONKEY",       #7 yes, in the original this id can be used to spawn donkeys, although it seems useless
+    "SPEC_SHEEP",           #8
 ]
 
 buildings = [
-    "BLD_WOODCUTTER", #0
-    "BLD_FORESTER", #1
-    "BLD_QUARRY", #2
-    "BLD_FISHERY", #3
-    "BLD_HUNTER", #4
-    "BLD_SAWMILL", #5
-    "BLD_FARM", #6
-    "BLD_PIGFARM", #7
-    "BLD_DONKEYBREEDER", #8
-    "BLD_SLAUGHTERHOUSE", #9
-    "BLD_MILL", #10
-    "BLD_BAKERY", #11
-    "BLD_IRONSMELTER", #12
-    "BLD_METALWORKS", #13
-    "BLD_ARMORY", #14
-    "BLD_MINT", #15
-    "BLD_SHIPYARD", #16
-    "BLD_BREWERY", #17
-    "BLD_GOLDMINE", #18
-    "BLD_IRONMINE", #19
-    "BLD_COALMINE", #20
-    "BLD_GRANITEMINE", #21
-    "BLD_STOREHOUSE", #22
-    "BLD_HARBORBUILDING", #23
-    "BLD_HEADQUARTERS", #24
-    "BLD_BARRACKS", #25
-    "BLD_GUARDHOUSE", #26
-    "BLD_WATCHTOWER", #27
-    "BLD_FORTRESS", #28
-    "BLD_LOOKOUTTOWER", #29
-    "BLD_WELL", #30
-    "BLD_CATAPULT", #31
-    "BLD_CHARBURNER", #32 this is actually a construction order for HQ in the original, whoever knew?
+    "BLD_WOODCUTTER",       #0
+    "BLD_FORESTER",         #1
+    "BLD_QUARRY",           #2
+    "BLD_FISHERY",          #3
+    "BLD_HUNTER",           #4
+    "BLD_SAWMILL",          #5
+    "BLD_FARM",             #6
+    "BLD_PIGFARM",          #7
+    "BLD_DONKEYBREEDER",    #8
+    "BLD_SLAUGHTERHOUSE",   #9
+    "BLD_MILL",             #10
+    "BLD_BAKERY",           #11
+    "BLD_IRONSMELTER",      #12
+    "BLD_METALWORKS",       #13
+    "BLD_ARMORY",           #14
+    "BLD_MINT",             #15
+    "BLD_SHIPYARD",         #16
+    "BLD_BREWERY",          #17
+    "BLD_GOLDMINE",         #18
+    "BLD_IRONMINE",         #19
+    "BLD_COALMINE",         #20
+    "BLD_GRANITEMINE",      #21
+    "BLD_STOREHOUSE",       #22
+    "BLD_HARBORBUILDING",   #23
+    "BLD_HEADQUARTERS",     #24
+    "BLD_BARRACKS",         #25
+    "BLD_GUARDHOUSE",       #26
+    "BLD_WATCHTOWER",       #27
+    "BLD_FORTRESS",         #28
+    "BLD_LOOKOUTTOWER",     #29
+    "BLD_WELL",             #30
+    "BLD_CATAPULT",         #31
+    "BLD_CHARBURNER",       #32 this is actually a construction order for HQ in the original, whoever knew?
 ]
 
 nations = [
-    "NAT_ROMANS",
-    "NAT_VIKINGS",
-    "NAT_JAPANESE",
-    "NAT_AFRICANS",
+    "NAT_ROMANS",           #0
+    "NAT_VIKINGS",          #1
+    "NAT_JAPANESE",         #2
+    "NAT_AFRICANS",         #3
 ]
 
 people = [
-    "JOB_HELPER",
-    "JOB_WOODCUTTER",
-    "JOB_FISHER",
-    "JOB_FORESTER",
-    "JOB_CARPENTER",
-    "JOB_STONEMASON",
-    "JOB_HUNTER",
-    "JOB_FARMER",
-    "JOB_MILLER",
-    "JOB_BAKER",
-    "JOB_BUTCHER",
-    "JOB_MINER",
-    "JOB_BREWER",
-    "JOB_PIGBREEDER",
-    "JOB_DONKEYBREEDER",
-    "JOB_IRONFOUNDER",
-    "JOB_MINTER",
-    "JOB_METALWORKER",
-    "JOB_ARMORER",
-    "JOB_BUILDER",
-    "JOB_PLANER",
-    "JOB_GEOLOGIST",
-    "JOB_PRIVATE",
-    "JOB_PRIVATEFIRSTCLASS",
-    "JOB_SERGEANT",
-    "JOB_OFFICER",
-    "JOB_GENERAL",
-    "JOB_SCOUT",
-    "JOB_SHIPWRIGHT",
-    "JOB_PACKDONKEY",
-    "JOB_BOATCARRIER",
-    "JOB_CHARBURNER",
+    "JOB_HELPER",           #0
+    "JOB_WOODCUTTER",       #1
+    "JOB_FISHER",           #2
+    "JOB_FORESTER",         #3
+    "JOB_CARPENTER",        #4
+    "JOB_STONEMASON",       #5
+    "JOB_HUNTER",           #6
+    "JOB_FARMER",           #7
+    "JOB_MILLER",           #8
+    "JOB_BAKER",            #9
+    "JOB_BUTCHER",          #10
+    "JOB_MINER",            #11
+    "JOB_BREWER",           #12
+    "JOB_PIGBREEDER",       #13
+    "JOB_DONKEYBREEDER",    #14
+    "JOB_IRONFOUNDER",      #15
+    "JOB_MINTER",           #16
+    "JOB_METALWORKER",      #17
+    "JOB_ARMORER",          #18
+    "JOB_BUILDER",          #19
+    "JOB_PLANER",           #20
+    "JOB_GEOLOGIST",        #21
+    "JOB_PRIVATE",          #22
+    "JOB_PRIVATEFIRSTCLASS",#23
+    "JOB_SERGEANT",         #24
+    "JOB_OFFICER",          #25
+    "JOB_GENERAL",          #26
+    "JOB_SCOUT",            #27
+    "JOB_SHIPWRIGHT",       #28
+    "JOB_PACKDONKEY",       #29
+    "JOB_BOATCARRIER",      #30
+    "JOB_CHARBURNER",       #31
 ]
         
 portraits = [
-    "Octavianus",
-    "Julius",
-    "Brutus",
-    "Erik",
-    "Knut",
-    "Olof",
-    "Yamauchi",
-    "Tsunami",
-    "Hakirawashi",
-    "Shaka",
-    "Todo",
-    "Mnga Tscha",
+    "Octavianus",           #0
+    "Julius",               #1
+    "Brutus",               #2
+    "Erik",                 #3
+    "Knut",                 #4
+    "Olof",                 #5
+    "Yamauchi",             #6
+    "Tsunami",              #7
+    "Hakirawashi",          #8
+    "Shaka",                #9
+    "Todo",                 #10
+    "Mnga Tscha",           #11
 ]
 
 wares = [
-    "GD_WOOD",
-    "GD_BOARDS",
-    "GD_STONES",
-    "GD_MEAT",
-    "GD_GRAIN",
-    "GD_FLOUR",
-    "GD_FISH",
-    "GD_HAM",
-    "GD_BREAD",
-    "GD_WATER",
-    "GD_BEER",
-    "GD_COAL",
-    "GD_IRONORE",
-    "GD_GOLD",
-    "GD_IRON",
-    "GD_COINS",
-    "GD_TONGS",
-    "GD_AXE",
-    "GD_SAW",
-    "GD_PICKAXE",
-    "GD_HAMMER",
-    "GD_SHOVEL",
-    "GD_CRUCIBLE",
-    "GD_RODANDLINE",
-    "GD_SCYTHE",
-    "GD_CLEAVER",
-    "GD_ROLLINGPIN",
-    "GD_BOW",
-    "GD_SWORD",
-    "GD_SHIELD",
-    "GD_BOAT",
+    "GD_WOOD",              #0
+    "GD_BOARDS",            #1
+    "GD_STONES",            #2
+    "GD_MEAT",              #3
+    "GD_GRAIN",             #4
+    "GD_FLOUR",             #5
+    "GD_FISH",              #6
+    "GD_HAM",               #7
+    "GD_BREAD",             #8
+    "GD_WATER",             #9
+    "GD_BEER",              #10
+    "GD_COAL",              #11
+    "GD_IRONORE",           #12
+    "GD_GOLD",              #13
+    "GD_IRON",              #14
+    "GD_COINS",             #15
+    "GD_TONGS",             #16
+    "GD_AXE",               #17
+    "GD_SAW",               #18
+    "GD_PICKAXE",           #19
+    "GD_HAMMER",            #20
+    "GD_SHOVEL",            #21
+    "GD_CRUCIBLE",          #22
+    "GD_RODANDLINE",        #23
+    "GD_SCYTHE",            #24
+    "GD_CLEAVER",           #25
+    "GD_ROLLINGPIN",        #26
+    "GD_BOW",               #27
+    "GD_SWORD",             #28
+    "GD_SHIELD",            #29
+    "GD_BOAT",              #30
 ]
