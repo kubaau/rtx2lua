a8e210660c12a12b75d93133ffabffb450a0a876
Jakub Audykowicz <jakub.audykowicz@gmail.com>
2024-07-18 02:09:38 +0200

    Add fanpaign data files

--- test.sh
+++ test.sh
@@ -6,6 +6,10 @@ do
     echo $f $?
     python rtx2lua.py $f campaign_name > /dev/null
     echo $f $?
-    python rtx2lua.py $f campaign_name TXT > /dev/null
+done
+
+for f in MISSIONS_fanpaign/*
+do
+    python rtx2lua.py $f fanpaign TXT_fanpaign > /dev/null
     echo $f $?
 done
