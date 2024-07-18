76bce20efa92a09422aaba31de7c4db52a28d658
Jakub Audykowicz <jakub.audykowicz@gmail.com>
2024-07-18 16:18:34 +0200

    Update test script

--- test.sh
+++ test.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
+rm -rf lua_roman
+mkdir -p lua_roman
+
+i=0
 for f in MISSIONS_roman/*
 do
-    mkdir -p lua_roman
-    python rtx2lua.py $f > lua_roman/$(basename ${f}).lua
+    python rtx2lua.py $f > lua_roman/MISS20${i}.lua
     echo $f $?
     python rtx2lua.py $f roman > /dev/null
     echo $f $?
+    ((i++))
 done
 
+rm -rf lua_fanpaign
+mkdir -p lua_fanpaign
+
+i=0
 for f in MISSIONS_fanpaign/*
 do
-    mkdir -p lua_fanpaign
-    python rtx2lua.py $f fanpaign TXT_fanpaign > lua_fanpaign/$(basename ${f}).lua
+    python rtx2lua.py $f fanpaign TXT_fanpaign > lua_fanpaign/MISS20${i}.lua
     echo $f $?
+    ((i++))
 done
